73d9e96a3816e91feaff1ac080bfda8248e59fc2
darya <darya.kolycheva98@yandex.ru>
2024-04-01 12:39:32 +0300

    ДЗ готово

--- src/Book.java
+++ src/Book.java
@@ -1,23 +1,50 @@
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author author;
     private int bookYear;
+
     public Book(String bookName, Author author, int bookYear) {
         this.bookName = bookName;
         this.author = author;
         this.bookYear = bookYear;
     }
-    public String getBookName () {
+
+    public String getBookName() {
         return this.bookName;
     }
-    public Author getAuthor () {
+
+    public Author getAuthor() {
         return this.author;
     }
-    public int getBookYear () {
+    public int getBookYear() {
         return this.bookYear;
     }
-    public void setBookYear (int bookYear) {
+
+    public void setBookYear(int bookYear) {
         this.bookYear = bookYear;
     }
 
+    public String toString() {
+        return String.valueOf(this.bookName + this.author + this.bookYear);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book that = (Book) o;
+        if (bookYear != that.bookYear) {
+            return false;
+        }
+        return Objects.equals(bookName, that.bookName);
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(bookName);
+    }
 }
